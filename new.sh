c5a9c6da4e8d3f228770d4671f0157c94cf1e322
roy4801 <a82611141@gmail.com>
2023-06-19 22:42:33 +0800

    update script

--- new.sh
+++ new.sh
@@ -4,7 +4,7 @@ OJ=$1
 
 if [[ "$1" == "" ]]; then
     echo "Usage: $0 <OJ>"
-    echo "  Supported OJ: leetcode"
+    echo "  Supported OJ: leetcode cses"
     exit 1
 fi
 
@@ -16,6 +16,14 @@ if [[ "$OJ" == "leetcode" ]]; then
     PID=$2
     PNAME="$3"
     DIFF="$4" # For leetcode
+elif [[ "$OJ" == "cses" ]]; then
+    if [[ "$2" == "" ]]; then
+        echo "Usage: $0 cses <problem ID> <problem name> <category>"
+        exit 1
+    fi
+    PID=$2
+    PNAME="$3"
+    CAT="$4" # For cses
 fi
 
 hexo new $OJ $PID > /dev/null
@@ -24,6 +32,11 @@ mv "./source/_posts/$PID.md" "./source/_posts/$OJ/$PID.md"
 TARGET="./source/_posts/$OJ/$PID.md"
 
 sed -i "s/PNAME/$PNAME/" $TARGET
-sed -i "s/CATEGORY/\[解題區, Leetcode, $DIFF\]/" $TARGET
+
+if [[ "$OJ" == "leetcode" ]]; then
+    sed -i "s/CATEGORY/\[解題區, Leetcode, $DIFF\]/" $TARGET
+elif [[ "$OJ" == "cses" ]]; then
+    sed -i "s/CATEGORY/\[解題區, CSES, $CAT\]/" $TARGET
+fi
 
 echo "Created: $TARGET"
---
+++ scaffolds/cses.md
@@ -0,0 +1,31 @@
+---
+title: CSES {{ title }} - PNAME
+categories:
+  - CATEGORY
+tags:
+  - 解題紀錄
+  - CSES
+date: {{ date }}
+---
+
+[題目]()
+
+# Problem
+
+
+## 測資限制
+
+
+# 想法
+
+- 時間複雜度: $\mathcal{O}()$
+- 空間複雜度: $\mathcal{O}()$
+
+# AC Code
+
+
+# 賞析
+
+
+# 心得
+
