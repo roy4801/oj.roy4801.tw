76d39b1e62fcb7bd3a4cd0577bc26c6fb6e38a74
roy4801 <a82611141@gmail.com>
2024-01-04 02:34:10 +0800

    update script

--- find_tag.sh
+++ find_tag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -inr "$1" --include ./source/_posts/\*.md
+grep -inr "$1" --include ./source/_posts/\*.md | grep -v '[題目]' | grep -v 'title:'
--- new.sh
+++ new.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+#set -xe
+
+leetcode_info=()
+function get_leetcode_info() {
+	pid=$1
+	difficulty=("Easy" "Medium" "Hard")
+	problem_set=`curl -sS https://leetcode.com/api/problems/algorithms/`
+	#problem=`echo $problem_set | jq '.stat_status_pairs[].stat | select( .frontend_question_id == 3)'`
+	problem=`echo $problem_set | jq ".stat_status_pairs[] | select( .stat.frontend_question_id == $pid )"`
+	
+	#echo $problem
+	
+	leetcode_info=()
+    # 0 = problem id
+    # 1 = difficulty
+    # 2 = problem title
+    # 3 = url
+	leetcode_info+=("$pid")
+	leetcode_info+=("${difficulty[`echo $problem | jq '.difficulty.level'`]}")
+	leetcode_info+=("`echo $problem | jq '.stat.question__title'`")
+
+	question_title_slug=`echo $problem | jq '.stat.question__title_slug'`
+	question_title_slug=${question_title_slug//\"/}
+	leetcode_info+=("https://leetcode.com/problems/${question_title_slug}")
+}
+
 OJ=$1
 
 if [[ "$1" == "" ]]; then
@@ -16,6 +42,13 @@ if [[ "$OJ" == "leetcode" ]]; then
     PID=$2
     PNAME="$3"
     DIFF="$4" # For leetcode
+
+    if [[ "${PNAME}" == "" ]]; then
+        get_leetcode_info $PID
+        PNAME=${leetcode_info[2]//\"/}
+        DIFF=${leetcode_info[1]}
+        URL="${leetcode_info[3]}"
+    fi
 elif [[ "$OJ" == "cses" ]]; then
     if [[ "$2" == "" ]]; then
         echo "Usage: $0 cses <problem ID> <problem name> <category>"
@@ -31,12 +64,13 @@ mv "./source/_posts/$PID.md" "./source/_posts/$OJ/$PID.md"
 
 TARGET="./source/_posts/$OJ/$PID.md"
 
-sed -i "s/PNAME/$PNAME/" $TARGET
+sed -i '' "s/PNAME/$PNAME/" $TARGET
 
 if [[ "$OJ" == "leetcode" ]]; then
-    sed -i "s/CATEGORY/\[解題區\, Leetcode\, $DIFF\]/" $TARGET
+    sed -i '' "s/CATEGORY/\[解題區\, Leetcode\, $DIFF\]/" $TARGET
+    sed -i '' "s|\[題目\]()|\[題目\]($URL)|g" $TARGET
 elif [[ "$OJ" == "cses" ]]; then
-    sed -i "s/CATEGORY/\[解題區\, CSES\, $CAT\]/" $TARGET
+    sed -i '' "s/CATEGORY/\[解題區\, CSES\, $CAT\]/" $TARGET
 fi
 
 echo "Created: $TARGET"
